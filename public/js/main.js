98ba3f4b85ccb31be68a189584125dd8722cc88d
Hassam Shamas <hassamq59@gmail.com>
2021-07-02 07:14:35 +0500

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,73 +1,23 @@
-$(window).ready(function () {
-  $('.navbar-phone').click(function (e) {
-    e.preventDefault();
-    $('.navbar-list').toggleClass('open');
-  })
+$(window).ready(function() {
+	$(".navbar-phone").click(function(a) {
+		a.preventDefault(), $(".navbar-list").toggleClass("open")
+	}), $(".navbar-item").click(function() {
+		$(".navbar-list").removeClass("open")
+	}), $(".go-back").click(function() {
+		let a = $("#index");
+		"0" == a.val() || (a.val(parseInt(a.val()) - 1), $(`.testimonial-image[data-index!=${a.val()}]`).attr("class", "testimonial-image close"), $(`.review-single[data-index!=${a.val()}]`).attr("class", "review-single close"), $(`.testimonial-image[data-index=${a.val()}]`).attr("class", "testimonial-image open"), $(`.review-single[data-index=${a.val()}]`).attr("class", "review-single open"))
+	}), $(".go-forward").click(function() {
+		let a = $("#index");
+		"2" == a.val() || (a.val(parseInt(a.val()) + 1), $(`.testimonial-image[data-index!=${a.val()}]`).attr("class", "testimonial-image close"), $(`.review-single[data-index!=${a.val()}]`).attr("class", "review-single close"), $(`.testimonial-image[data-index=${a.val()}]`).attr("class", "testimonial-image open"), $(`.review-single[data-index=${a.val()}]`).attr("class", "review-single open"))
+	}), setInterval(() => {
+		let a = $("#index");
+		a.val(parseInt(a.val()) + 1), "3" == a.val() && a.val(0), $(`.testimonial-image[data-index!=${a.val()}]`).attr("class", "testimonial-image close"), $(`.review-single[data-index!=${a.val()}]`).attr("class", "review-single close"), $(`.testimonial-image[data-index=${a.val()}]`).attr("class", "testimonial-image open"), $(`.review-single[data-index=${a.val()}]`).attr("class", "review-single open")
+	}, 6e3)
+}), window.onscroll = onScroll;
+var navbar = document.getElementById("navbar"),
+	cover = document.getElementById("cover"),
+	sticky = navbar.offsetTop;
 
-  $('.navbar-item').click(function () {
-    $('.navbar-list').removeClass('open');
-  });
-
-  $('.go-back').click(function (e) {
-    let index = $('#index');
-    if (index.val() == '0') return ;
-
-    index.val(parseInt(index.val()) - 1);
-    
-    $(`.testimonial-image[data-index!=${index.val()}]`).attr('class', 'testimonial-image close')
-    $(`.review-single[data-index!=${index.val()}]`).attr('class', 'review-single close')
-
-    
-    $(`.testimonial-image[data-index=${index.val()}]`).attr('class', 'testimonial-image open')
-    $(`.review-single[data-index=${index.val()}]`).attr('class', 'review-single open')
-  })
-
-  $('.go-forward').click(function (e) {
-    let index = $('#index');
-    if (index.val() == '2') return ;
-    
-    index.val(parseInt(index.val()) + 1);
-
-    
-    $(`.testimonial-image[data-index!=${index.val()}]`).attr('class', 'testimonial-image close')
-    $(`.review-single[data-index!=${index.val()}]`).attr('class', 'review-single close')
-
-    $(`.testimonial-image[data-index=${index.val()}]`).attr('class', 'testimonial-image open')
-    $(`.review-single[data-index=${index.val()}]`).attr('class', 'review-single open')
-  });
-
-  setInterval(() => {
-    let index = $('#index');
-    index.val(parseInt(index.val()) + 1);
-
-    if (index.val() == '3') index.val(0);
-
-    $(`.testimonial-image[data-index!=${index.val()}]`).attr('class', 'testimonial-image close')
-    $(`.review-single[data-index!=${index.val()}]`).attr('class', 'review-single close')
-
-    $(`.testimonial-image[data-index=${index.val()}]`).attr('class', 'testimonial-image open')
-    $(`.review-single[data-index=${index.val()}]`).attr('class', 'review-single open')
-  }, 6000)
-})
-
-window.onscroll = onScroll;
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-var cover = document.getElementById("cover");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function onScroll() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    // cover.classList.add("sticky")
-  }
-  
-  if (window.pageYOffset < 2) {
-    navbar.classList.remove("sticky");
-    // cover.classList.remove("sticky");
-  }
+	window.pageYOffset >= sticky && navbar.classList.add("sticky"), 2 > window.pageYOffset && navbar.classList.remove("sticky")
 }
